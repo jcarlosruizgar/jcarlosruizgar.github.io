c37a28b4ead05eaa7e3c75a5ac33780e40feba9c
jcarlosruizgar <jcarlosruizgar@gmail.com>
2022-05-24 23:51:09 +0200

    update

--- js/hola.js
+++ js/hola.js
@@ -1,9 +1,18 @@
-//hacer el login
+var hoy = new Date();
+var hora = hoy.getHours();
+var mensaje;
+if(hora>=6&hora<12){
+    mensaje = "Buenos días, ";
+}
+else if(hora>=12&hora<21){
+    mensaje = "Buenas tardes, ";
+}
+else{
+    mensaje = "Buenas noches, ";
+}
 var data = [];
-$("#formulario_contacto").submit(function(event) {
+$("#formulario_login").submit(function(event) {
     event.preventDefault();
-    data.push($(this).find('input[name="nombre"]').val());
-    data.push($(this).find('input[name="apellidos"]').val());
-    //alert("Formulario enviado con éxito.");
-    document.getElementById("validacion_formulario").innerHTML = data[1];
+    data.push($(this).find('input[name="nombre_login"]').val());
+    document.getElementById("b_usr").innerHTML = mensaje+data[0];
 });
